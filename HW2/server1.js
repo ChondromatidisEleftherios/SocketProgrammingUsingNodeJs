b03246fec54f32b20a227971587dc0e03162b38b
Chondromatidis Eleftherios <148573871+ChondromatidisEleftherios@users.noreply.github.com>
2025-11-01 19:05:48 +0200

    01/11/2025

---
+++ HW2/server1.js
@@ -0,0 +1,36 @@
+//@ts-check
+("use strict");
+
+
+import * as net from "net";
+
+(function main() {
+	const portNum = 2300;
+	const loopbackAddress = "127.0.0.1";
+	const msg = "Hello World! \n";
+	const server = net.createServer(writeToClient);
+	const meow = 10;
+	server.maxConnections = 1;
+
+	function writeToClient(socket) {
+		console.log(`Incoming connection from ${socket.remoteAddress}, sending Welcome!`);
+		socket.write(msg);
+		socket.on("error", handleError);
+		server.close(serverShutDown);
+	}
+
+	server.listen({ port: portNum, host: loopbackAddress }, portAndIpInfo(portNum, loopbackAddress));
+
+	function serverShutDown() {
+		console.log("Server Shutting Down...");
+	}
+
+	function handleError(err) {
+		console.log(`Error: ${err}`);
+	}
+
+	function portAndIpInfo(portNum, loopbackAddress) {
+		console.log(`Server is Listening on port: ${portNum} and is connected to host: ${loopbackAddress}`);
+	}
+
+}());
